eba02790e6ded65339417b171bde72dcfefb398e
Amazingbabs <babatundeagbeyisanmi@gmail.com>
2023-09-20 11:31:14 -0700

    codes on task o with file name 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include “msin.h”
+/**
+ * Strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ * Return: void
+ */
+Char *_strcat(char *dest, char *src)
+{
+	Int i;
+	Int j;
+	I = 0
+	While (dest[i] != ‘\0’)
+	{
+		I++;
+}
+J = 0;
+While (src[j] != ‘\0’)
+{
+	Dest[i] = src[j];
+	I++;
+	J++;
+}
+Dest[i] = ‘\0’;
+Return (dest);
+}
+
