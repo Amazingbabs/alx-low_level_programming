a180e1911e31103a6d34c320ad093d954f4d4b08
Amazingbabs <babatundeagbeyisanmi@gmail.com>
2023-09-20 10:35:57 -0700

    main.h

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+Int _putchar(char c);
+Char *_strcat(char *dest, char *src);
+Char *_strncat(char *dest, char *src, int n);
+Char *_strncpy(char *dest, char *src, int n);
+Int _strcap(char *s1, char *s2);
+Void reverse_array(int *a, int n);
+Char *string_toupper(char *a);
+Char *cap_string(char *a);
+Char *leet(char *a);
+Char *rot13(char *a);
+Void print_number(int n);
+Char *infinite_add(char *n1, char *n2, char *r, int size_r);
+Void print_buffer(char *b, int size);
+#endif
+
